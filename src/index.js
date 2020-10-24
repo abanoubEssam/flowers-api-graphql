0f01425f641a70bfde35d890fbf0ee880d7753f3
Abanoub Essam <20130130@fa-hists.edu.eg>
2020-10-24 14:40:37 +0200

    work on serving imgs

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
-const { ApolloServer, gql } = require("apollo-server");
+const { ApolloServer, gql } = require("apollo-server-express");
 const mongoose = require("mongoose");
 const { mongodbURL } = require("./constants");
 const { userMutations } = require("./graphql/resolvers/mutations/user/user.mutations");
 const { userTypeDef } = require("./graphql/type-definitions/user/user.typedef");
 import express from 'express';
-import { join } from 'path';
+import path from 'path';
 import { userQueries } from './graphql/resolvers/queries/user/user.queries';
 import { validateToken } from './middlewares/jwt.service';
 
 const app = express()
-app.use('/uploads', express.static(join(__dirname, '../uploads')))
+const uploadPath = path.resolve(__dirname, '../uploads')
+app.use('/uploads', express.static(uploadPath))
 
+console.log("uploadPath", uploadPath)
 const server = new ApolloServer({
     typeDefs: [
         userTypeDef
@@ -29,22 +31,29 @@ const server = new ApolloServer({
     context: async ({ req }) => {
         // console.log("req.body", req.body)
         if (req.headers.authorization) {
-               return await validateToken(req.headers.authorization)
+            return await validateToken(req.headers.authorization)
         }
     },
     // playground: false
 
 })
 
-const port = process.env.PORT || 8000
+const port = process.env.PORT || 3000
 
-server.listen({ port }).then(async res => {
-    try {
-        await mongoose.connect(mongodbURL, { useNewUrlParser: true, useUnifiedTopology: true })
-        console.log(`db connected to ${mongodbURL}`)
-    } catch (error) {
-        console.log("error", error)
+server.applyMiddleware({ app })
 
-    }
-    console.log(`SERVER IS UP ${res.url}`);
-})
+app.listen({ port }, async () => {
+    await mongoose.connect(mongodbURL, { useNewUrlParser: true, useUnifiedTopology: true })
+    console.log(`db connected to ${mongodbURL}`)
+    console.log(`🚀 Server ready at http://localhost:${port}${server.graphqlPath}`)
+
+}
+)
+// app.listen({ port }).then(async res => {
+//     try {
+//     } catch (error) {
+//         console.log("error", error)
+
+//     }
+//     console.log(`SERVER IS UP ${res.url}`);
+// })
