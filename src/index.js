8aa15c9a48ba8d4d404450bb2b614e7544c44211
Abanoub Essam <20130130@fa-hists.edu.eg>
2020-10-24 23:55:39 +0200

    add config lib

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import express from 'express';
 import path from 'path';
 import { userQueries } from './graphql/resolvers/queries/user/user.queries';
 import { validateToken } from './middlewares/jwt.service';
-
+import config from 'config'
 const app = express()
 const uploadPath = path.resolve(__dirname, '../uploads')
 const publicPath = path.resolve(__dirname, '../public')
@@ -53,7 +53,7 @@ const port = process.env.PORT || 3000
 server.applyMiddleware({ app })
 
 app.listen({ port }, async () => {
-    await mongoose.connect(mongodbURL, { useNewUrlParser: true, useUnifiedTopology: true })
+    await mongoose.connect(config.get('mongodbURL'), { useNewUrlParser: true, useUnifiedTopology: true })
     console.log(`db connected to ${mongodbURL}`)
     console.log(`🚀 Server ready at http://localhost:${port}${server.graphqlPath}`)
 
